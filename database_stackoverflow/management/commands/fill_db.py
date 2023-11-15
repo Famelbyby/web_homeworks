90c0dc810987219055600a6e95cff623f86cbeb2
Alex <130932460+Famelbyby@users.noreply.github.com>
2023-11-15 19:54:06 +0300

    Update fill_db.py

--- database_stackoverflow/management/commands/fill_db.py
+++ database_stackoverflow/management/commands/fill_db.py
@@ -26,18 +26,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         ratio = options['ratio'][0]
-        for i in range(1, ratio * 100 + 1):
-            if i % 10000 == 0:
-                print(i)
-            Answer.objects.create(answer_id=i,
-                                  description=get_rand_str(1, 50, True, True),
-                                  date=timezone.now(),
-                                  question_id=Question.objects.all()[randint(0, ratio * 10 - 1)],
-                                  author_id=Profile.objects.all()[randint(0, ratio - 1)],
-                                  rating=randint(0, ratio - 1)
-            )
-        print('Created answers')
-        return
         for i in range(1, ratio + 1):
             u = User(id=i,
                      username=get_rand_str(10, 20),
@@ -63,4 +51,15 @@ class Command(BaseCommand):
             for i in range(randint(1, 3)):
                 q.tags.add(Tag.objects.all()[randint(0, ratio - 1)])
         print('Created questions')
+        for i in range(1, ratio * 100 + 1):
+            if i % 10000 == 0:
+                print(i)
+            Answer.objects.create(answer_id=i,
+                                  description=get_rand_str(1, 50, True, True),
+                                  date=timezone.now(),
+                                  question_id=Question.objects.all()[randint(0, ratio * 10 - 1)],
+                                  author_id=Profile.objects.all()[randint(0, ratio - 1)],
+                                  rating=randint(0, ratio - 1)
+            )
+        print('Created answers')
 
